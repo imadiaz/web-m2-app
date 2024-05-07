d83203510e566c4ffcda7d11d7228ba68eca4ba0
isai3 <20223tn137@utez.edu.mx>
2024-05-07 15:32:11 -0600

    Just added react-router-dom dependency and few components on main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,15 @@ import App from './App'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './core/store'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <Provider store={store}>
-    <App />
+    <BrowserRouter>
+    <Routes>
+        <Route path='/*' element={<App />}/>
+    </Routes>
+    </BrowserRouter>   
     </Provider>
 )
