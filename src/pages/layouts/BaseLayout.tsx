d07057f1925cf12095c9c0e169c13bbac98ff04b
isai3 <20223tn137@utez.edu.mx>
2024-05-08 17:51:22 -0600

    This commit change the BaseLayout.tsx content  and adds BaseLayoutStyles.ts file

--- src/pages/layouts/BaseLayout.tsx
+++ src/pages/layouts/BaseLayout.tsx
@@ -1,85 +1,65 @@
-import React, { useState } from 'react';
-import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
-import type { MenuProps } from 'antd';
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import React, { useState } from "react";
+import { Layout, Menu, Button, theme } from "antd";
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+} from "@ant-design/icons";
+import { buttonSiderStyle, contentStyle, headerStyle, layoutStyle } from "./BaseLayoutStyles";
 
-const { Header, Content, Sider } = Layout;
+const { Header, Sider, Content } = Layout;
 
-const items1: MenuProps['items'] = ['1', '2', '3'].map((key) => ({
-  key,
-  label: `nav ${key}`,
-}));
-
-const items2: MenuProps['items'] = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
-  (icon, index) => {
-    const key = String(index + 1);
-
-    return {
-      key: `sub${key}`,
-      icon: React.createElement(icon),
-      label: `subnav ${key}`,
-
-      children: new Array(4).fill(null).map((_, j) => {
-        const subKey = index * 4 + j + 1;
-        return {
-          key: subKey,
-          label: `option${subKey}`,
-        };
-      }),
-    };
-  },
-);
-
-const App: React.FC = () => {
+const BaseLayout: React.FC = () => {
+  const [collapsed, setCollapsed] = useState(false);
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
 
-  const [collapsed, setCollapsed] = useState(false);
-
   return (
-    <Layout style={{minHeight: '100vh'}}>
-      <Header style={{ display: 'flex', alignItems: 'center' }}>
+    <Layout style={layoutStyle}>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
         <Menu
           theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={['2']}
-          items={items1}
-          style={{ flex: 1, minWidth: 0}}
+          mode="inline"
+          defaultSelectedKeys={["1"]}
+          items={[
+            {
+              key: "1",
+              icon: <UserOutlined />,
+              label: "page 1",
+            },
+            {
+              key: "2",
+              icon: <VideoCameraOutlined />,
+              label: "page 2",
+            },
+            {
+              key: "3",
+              icon: <UploadOutlined />,
+              label: "page 3",
+            },
+          ]}
         />
-      </Header>
+      </Sider>
       <Layout>
-        <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)} width={200} style={{ background: colorBgContainer }}>
-          <Menu
-            theme='dark'
-            mode="inline"
-            defaultSelectedKeys={['1']}
-            defaultOpenKeys={['sub1']}
-            style={{ height: '100%', borderRight: 0, background: '#191919'}}
-            items={items2}
+        <Header style={headerStyle(colorBgContainer)}>
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={buttonSiderStyle}
           />
-        </Sider>
-        <Layout style={{ padding: '0 24px 24px' }}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
-          </Breadcrumb>
-          <Content
-            style={{
-              padding: 24,
-              margin: 0,
-              minHeight: 280,
-              background: colorBgContainer,
-              borderRadius: borderRadiusLG,
-            }}
-          >
-            Content
-          </Content>
-        </Layout>
+        </Header>
+        <Content
+          style={contentStyle(colorBgContainer, borderRadiusLG)}
+        >
+          Content
+        </Content>
       </Layout>
     </Layout>
   );
 };
 
-export default App;
+export default BaseLayout;
---
+++ src/pages/layouts/BaseLayoutStyles.ts
@@ -0,0 +1,21 @@
+export const layoutStyle = {
+  minHeight: "100vh",
+};
+export const headerStyle = (colorBgContainer: string) => ({
+  padding: 0,
+  background: colorBgContainer,
+});
+export const buttonSiderStyle = {
+  fontSize: "16px",
+  width: 64,
+  height: 64,
+};
+export const contentStyle = (
+  colorBgContainer: string,
+  borderRadiusLG: number
+) => ({
+  margin: "24px 16px",
+  padding: 10,
+  background: colorBgContainer,
+  borderRadius: borderRadiusLG,
+});
