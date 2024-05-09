e4974f96a125c945efec4cb28b59be19346adb58
Caleb-Nieto <133397335+Caleb-Nieto@users.noreply.github.com>
2024-05-09 10:59:02 -0600

    react-router-dom (#5)
    * Just added react-router-dom dependency and few components on main.tsx file
    
    * Router navigation implemented with some proof pages. Added Routes.tsx file
    
    * Route class moved into /routes/models/Route.tsx file

--- src/pages/routes/Routes.tsx
+++ src/pages/routes/Routes.tsx
@@ -1,5 +1,10 @@
 import { ItemType } from "antd/es/menu/hooks/useItems";
+<<<<<<< HEAD
 import { Page2, Page3 } from "../proofPages/proofPages";
+=======
+import { Page1, Page2, Page3 } from "../proofPages/proofPages";
+<<<<<<< HEAD
+>>>>>>> 39f4344 (react-router-dom (#5))
 import { VideoCameraOutlined, UploadOutlined } from "@ant-design/icons";
 import { MenuProps } from "antd";
 import { Route } from "./models/Route";
@@ -15,6 +20,22 @@ const companies = new Route(
   Routes.AdminDirectionHome,
   <Company />,
   <BsBuildings />
+=======
+import {
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+} from "@ant-design/icons";
+import { MenuProps } from "antd";
+import { Route } from "./models/Route";
+
+const page1 = new Route(
+  "Page 1",
+  "page1",
+  "/page1",
+  <Page1 />,
+  <UserOutlined />
+>>>>>>> 049089b (react-router-dom (#5))
 );
 const page2 = new Route(
   "Page 2",
@@ -31,17 +52,26 @@ const page3 = new Route(
   <UploadOutlined />
 );
 
+<<<<<<< HEAD
 const adminRoutes: Route[] = [companies, page2, page3];
 
 const adminRoutesSiderOptions = (): ItemType[] => {
   const items: MenuProps["items"] = [
     getItem(companies.label, companies.fullPath, companies.icon),
+=======
+const proofRoutes: Route[] = [page1, page2, page3];
+
+const proofPagesSiderOptions = (): ItemType[] => {
+  const items: MenuProps["items"] = [
+    getItem(page1.label, page1.fullPath, page1.icon),
+>>>>>>> 049089b (react-router-dom (#5))
     getItem(page2.label, page2.fullPath, page2.icon),
     getItem(page3.label, page3.fullPath, page3.icon),
   ];
   return items;
 };
 
+<<<<<<< HEAD
 const getUserSiderOptions = (user: User): ItemType[] => {
   const rol = getUserRol(user);
   if (rol == UserRoles.ADMIN) {
@@ -53,6 +83,11 @@ const getUserSiderOptions = (user: User): ItemType[] => {
 export { adminRoutesSiderOptions, adminRoutes, getUserSiderOptions };
 
 function getItem(
+=======
+export type MenuItem = Required<MenuProps>["items"][number];
+
+export function getItem(
+>>>>>>> 049089b (react-router-dom (#5))
   label: React.ReactNode,
   key: React.Key,
   icon?: React.ReactNode,
@@ -68,4 +103,8 @@ function getItem(
   } as MenuItem;
 }
 
+<<<<<<< HEAD
 type MenuItem = Required<MenuProps>["items"][number];
+=======
+export { proofPagesSiderOptions, proofRoutes };
+>>>>>>> 049089b (react-router-dom (#5))
