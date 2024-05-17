9342a3740bfaaba321b987f0f9032ab9027fdf0d
Caleb-Nieto <133397335+Caleb-Nieto@users.noreply.github.com>
2024-05-17 09:53:49 -0600

    Sider options for admin (#8)

--- src/pages/routes/Routes.tsx
+++ src/pages/routes/Routes.tsx
@@ -4,6 +4,7 @@ import { ItemType } from "antd/es/menu/hooks/useItems";
 import { Page1, Page2, Page3 } from "../proofPages/proofPages";
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 import { Page2, Page3 } from "../proofPages/proofPages";
 >>>>>>> 5972a9a (Get Companies responsive (#15))
@@ -54,16 +55,27 @@ const companies = new Route(
   <BsBuildings />
 >>>>>>> 9e42fb8 (Sider options for admin (#8))
 =======
+=======
+import { VideoCameraOutlined, UploadOutlined } from "@ant-design/icons";
+>>>>>>> 9e42fb8 (Sider options for admin (#8))
 import { MenuProps } from "antd";
 import { Route } from "./models/Route";
+import User from "../../data/user/user";
+import { UserRoles, getUserRol } from "../../utils/Extensions";
+import { BsBuildings } from "react-icons/bs";
+import Routes from "../../utils/Routes";
 
-const page1 = new Route(
-  "Page 1",
-  "page1",
-  "/page1",
+const companies = new Route(
+  "Companies",
+  "companies",
+  Routes.AdminDirectionHome,
   <Page1 />,
+<<<<<<< HEAD
   <UserOutlined />
 >>>>>>> 049089b (react-router-dom (#5))
+=======
+  <BsBuildings />
+>>>>>>> 9e42fb8 (Sider options for admin (#8))
 );
 const page2 = new Route(
   "Page 2",
@@ -83,6 +95,7 @@ const page3 = new Route(
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 const adminRoutes: Route[] = [companies, page2, page3];
 
 const adminRoutesSiderOptions = (): ItemType[] => {
@@ -104,11 +117,18 @@ const adminRoutesSiderOptions = (): ItemType[] => {
 >>>>>>> 9e42fb8 (Sider options for admin (#8))
 =======
 const proofRoutes: Route[] = [page1, page2, page3];
+=======
+const adminRoutes: Route[] = [companies, page2, page3];
+>>>>>>> 9e42fb8 (Sider options for admin (#8))
 
-const proofPagesSiderOptions = (): ItemType[] => {
+const adminRoutesSiderOptions = (): ItemType[] => {
   const items: MenuProps["items"] = [
+<<<<<<< HEAD
     getItem(page1.label, page1.fullPath, page1.icon),
 >>>>>>> 049089b (react-router-dom (#5))
+=======
+    getItem(companies.label, companies.fullPath, companies.icon),
+>>>>>>> 9e42fb8 (Sider options for admin (#8))
     getItem(page2.label, page2.fullPath, page2.icon),
     getItem(page3.label, page3.fullPath, page3.icon),
   ];
@@ -118,6 +138,9 @@ const proofPagesSiderOptions = (): ItemType[] => {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 9e42fb8 (Sider options for admin (#8))
 =======
 >>>>>>> 9e42fb8 (Sider options for admin (#8))
 const getUserSiderOptions = (user: User): ItemType[] => {
@@ -128,6 +151,7 @@ const getUserSiderOptions = (user: User): ItemType[] => {
   return [];
 };
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 export { adminRoutesSiderOptions, adminRoutes, getUserSiderOptions };
 
@@ -148,6 +172,12 @@ export type MenuItem = Required<MenuProps>["items"][number];
 
 export function getItem(
 >>>>>>> 049089b (react-router-dom (#5))
+=======
+
+export { adminRoutesSiderOptions, adminRoutes, getUserSiderOptions };
+
+function getItem(
+>>>>>>> 9e42fb8 (Sider options for admin (#8))
   label: React.ReactNode,
   key: React.Key,
   icon?: React.ReactNode,
@@ -166,6 +196,7 @@ export function getItem(
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 type MenuItem = Required<MenuProps>["items"][number];
 =======
 export { proofPagesSiderOptions, proofRoutes };
@@ -176,3 +207,6 @@ type MenuItem = Required<MenuProps>["items"][number];
 =======
 export { proofPagesSiderOptions, proofRoutes };
 >>>>>>> 049089b (react-router-dom (#5))
+=======
+type MenuItem = Required<MenuProps>["items"][number];
+>>>>>>> 9e42fb8 (Sider options for admin (#8))
