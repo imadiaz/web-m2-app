b9bf4b70516b098b5b1531fe1e30f0d6c39de9c0
Caleb-Nieto <133397335+Caleb-Nieto@users.noreply.github.com>
2024-06-06 17:40:23 -0600

    Create card type (#40)

--- src/services/CardTypesService.ts
+++ src/services/CardTypesService.ts
@@ -1,4 +1,5 @@
 import { CardTypes } from "../data/cardtypes/cardTypes";
+import { CreateCardType } from "../data/cardtypes/cardTypes.request";
 import { apiSlice } from "./apiSlice";
 
 export const cardTypesService = apiSlice.injectEndpoints({
@@ -6,8 +7,15 @@ export const cardTypesService = apiSlice.injectEndpoints({
         getCardTypes: builder.mutation<CardTypes[], string>({
             query: (siteId) => `/cardTypes/all/${siteId}`,
             transformResponse: (response: {data: CardTypes[]}) => response.data
+        }),
+        createCardType: builder.mutation<void, CreateCardType>({
+            query: (cardType) => ({
+                url: '/cardTypes/create',
+                method: 'POST',
+                body: {...cardType}
+            })
         })
     })
 })
 
-export const {useGetCardTypesMutation} = cardTypesService
+export const {useGetCardTypesMutation, useCreateCardTypeMutation} = cardTypesService
