ed131840c398a505bd8d268fd7534aa95e5e84e0
isai3 <20223tn137@utez.edu.mx>
2024-06-06 17:32:49 -0600

    logo

--- src/data/user/user.ts
+++ src/data/user/user.ts
@@ -3,11 +3,19 @@ export default class User {
   email: string;
   token: string;
   roles: string[];
+  logo: string;
 
-  constructor(name: string, email: string, token: string, roles: string[]) {
+  constructor(
+    name: string,
+    email: string,
+    token: string,
+    roles: string[],
+    logo: string
+  ) {
     this.name = name;
     this.email = email;
     this.token = token;
     this.roles = roles;
+    this.logo = logo;
   }
 }
